dab2d45f53ee22dd66d2063cf9bfc4e79512f489
Koladele Olaitan <koladeleolaitan@gmail.com>
2022-06-20 10:22:09 +0000

    Day 5: Added slider functionality on homepage.

--- js/index.js
+++ js/index.js
@@ -38,3 +38,50 @@ const observer = new IntersectionObserver(
 );
 
 observer.observe(statisticsSection);
+
+// carousel slider
+const carouselSlide = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+
+// carousel buttons
+const nextSlideButton = document.querySelector(".next-slide");
+const prevSlideButton = document.querySelector(".prev-slide");
+
+// counter and size
+let counter = 1;
+const slideWidth = slides[0].clientWidth;
+carouselSlide.style.transform = `translateX(${-slideWidth * counter}px)`;
+
+// next slide function
+
+function nextSlide() {
+  if (counter >= slides.length - 1) return;
+  carouselSlide.style.transition = `transform 0.4s ease-in-out`;
+  counter++;
+  carouselSlide.style.transform = `translateX(${-slideWidth * counter}px)`;
+}
+
+function prevSlide() {
+  if (counter <= 0) return;
+  carouselSlide.style.transition = `transform 0.4s ease-in-out`;
+  counter--;
+  carouselSlide.style.transform = `translateX(${-slideWidth * counter}px)`;
+}
+
+nextSlideButton.addEventListener("click", nextSlide);
+prevSlideButton.addEventListener("click", prevSlide);
+setInterval(nextSlide, 3000);
+
+// loop back to first slide
+carouselSlide.addEventListener("transitionend", () => {
+  if (slides[counter].classList.contains("lastClone")) {
+    carouselSlide.style.transition = "none";
+    counter = slides.length - 2;
+    carouselSlide.style.transform = `translateX(${-slideWidth * counter}px)`;
+  }
+  if (slides[counter].classList.contains("firstClone")) {
+    carouselSlide.style.transition = "none";
+    counter = slides.length - counter;
+    carouselSlide.style.transform = `translateX(${-slideWidth * counter}px)`;
+  }
+});
